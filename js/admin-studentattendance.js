1cb0fc6ce1287da76ba8faa7b677ceb2a0af4175
BinodMagar60 <kauchabinod88@gmail.com>
2023-12-23 10:17:53 +0545

    4th commit

---
+++ js/admin-studentattendance.js
@@ -0,0 +1,15 @@
+
+function studentAttendance(){
+    const xhr = new XMLHttpRequest();
+    const container = document.getElementById('container');
+    
+    xhr.onload = function() {
+      if (this.status === 200) {
+        container.innerHTML = xhr.responseText;
+      } else {
+        console.warn("Did not receive 200 OK from response!");
+      }
+    };
+    xhr.open('GET', 'admin-studentattendance.html');
+    xhr.send();
+  }
