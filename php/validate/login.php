7da7376ecf312412ce2e412f8725c7388b9f5e1e
Thaman <thaman@gmail.com>
2023-12-24 12:47:49 +0545

    password fix

--- php/validate/login.php
+++ php/validate/login.php
@@ -1,4 +1,17 @@
 <?php
 require_once "../config/db.php";
+if(isset($_POST['login-submit'])){
+$cemail=$_POST['email'];
+$cpassword=$_POST['password'];
+
+}
+
+$sql="select * from student_table where email='$cemail')";
+$exesql=mysqli_query($con,$sql);
+if($cemail==$exesql['email']){
+  if(password_verify($cpassword,$exesql['password_hash'])){
+    
+  }
+}
 
 ?>
