27451db0329c0a08b258379825cedbfdb81abeb9
Thaman <thaman@gmail.com>
2023-12-24 09:53:38 +0545

    Password and password hash

--- php/add/student_add.php
+++ php/add/student_add.php
@@ -7,6 +7,7 @@ $sgender=$_POST['gender'];
 $sdob=$_POST['s-dob'];
 $scontact=$_POST['s-contact'];
 $smail=$_POST['s-mail'];
+$spassword=$_POST['s-password'];
 $simage=$_POST['s-image'];
 $sfather=$_POST['s-father'];
 $smother=$_POST['s-mother'];
@@ -31,13 +32,17 @@ $ssection
 ";
 */
 if(isset($sname,$saddress,$sgender,$sdob,$scontact,$smail,$simage,$sfather,$smother,$sparentcontact,$sclass,$ssection)){
-$sql= "INSERT INTO student_table(name,address, gender, date_of_birth, contact, email, image, father_name, mother_name,parents_contact, class, section) VALUES ('$sname','$saddress','$sgender','$sdob','$scontact','$smail','$simage','$sfather','$smother','$sparentcontact','$sclass','$ssection')";
+  //secure password
+  $secure_password=password_hash($spassword,PASSWORD_DEFAULT);
+$sql= "INSERT INTO student_table(name,address, gender, date_of_birth, contact, email, password_hash, image, father_name, mother_name,parents_contact, class, section) VALUES ('$sname','$saddress','$sgender','$sdob','$scontact','$smail','$secure_password','$simage','$sfather','$smother','$sparentcontact','$sclass','$ssection')";
 
 if(mysqli_query($con,$sql)){
-  echo "data inserted successfully";
+  // echo "data inserted successfully";
 }else{
-  echo "error";
+  // echo "error";
 }
 }
 }
+header("location: ../../admin.html");
+exit();
 ?>
