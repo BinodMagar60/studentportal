76b1200e66b89c0875989314a8185d872f7ec582
ThamanGurung01 <tgurung519@gmail.com>
2023-12-28 09:51:47 +0545

    validation update1

--- php/validate/validate.php
+++ php/validate/validate.php
@@ -3,6 +3,7 @@ require_once "../config/folder.php";
 // Initialize variables
 $name = $address = $gender = $date_of_birth = $contact = $email =$password = $father_name = $mother_name = $parents_contact = $class = $section = $imageFile = "";
 $errors = [];
+require_once "fileValidate.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //use ucwords
@@ -10,14 +11,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['s-name'])) {
         $errors["name"] = "Name is required";
     } else {
-        $sname = test_input($_POST["s-name"]);
+        $sname = ucwords(test_input($_POST["s-name"]));
     }
 
     // Validate Address
     if (empty($_POST["s-address"])) {
         $errors["address"] = "Address is required";
     } else {
-        $saddress = test_input($_POST["s-address"]);
+        $saddress = ucwords(test_input($_POST["s-address"]));
     }
 
     // Validate Gender
@@ -51,55 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-       // Validate Image File
-        if (!empty($_FILES["s-photo"])) {
-            if($_FILES['s-photo']['error']===0){
-            $imageFile = $_FILES["s-photo"];
-            $imageName= $_FILES['s-photo']['name'];
-            $imageTmp=$_FILES['s-photo']['tmp_name'];
-            $imageType=$_FILES['s-photo']['type'];
-            $imageSize=$_FILES['s-photo']['size'];
-            $imageError=$_FILES['s-photo']['error'];
-
-                $imageExt=explode('.',$imageName);
-                $ActualImageExt=strtolower(end($imageExt));
-                $imageNewName=uniqid().".".$ActualImageExt;
-                    if($imageError===0){
-                if(move_uploaded_file($imageTmp,$imageSavePath."/".$imageNewName)){
-                    $simage=$image_save.$imageNewName;
-                }else{
-                    echo "error uploading files";
-                }
-            }
-            }
-
-
-
-            // $allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-            // $maxFileSize = 2 * 1024 * 1024;
-    
-            // Check file type
-            // if (!in_array($imageFile["type"], $allowedTypes)) {
-            //     $errors["imagefile"] = "Invalid file type. Allowed types: JPEG, PNG, GIF";
-            // }
-    
-            // Check file size
-            // if ($imageFile["size"] > $maxFileSize) {
-            //     $errors["imagefile"] = "File size exceeds the maximum allowed size (2 MB)";
-            // }
-    
-            // Check image dimensions (optional)
-            // Example: Check if the image is at least 300x300 pixels
-            // $imageDimensions = getimagesize($imageFile["tmp_name"]);
-            // $minWidth = 300;
-            // $minHeight = 300;
-    
-            // if ($imageDimensions[0] < $minWidth || $imageDimensions[1] < $minHeight) {
-            //     $errors["imagefile"] = "Image dimensions must be at least $minWidth x $minHeight pixels";
-            // }
-        } else {
-            $errors["imagefile"] = "Image file is required";
-        }
+       
 
 //validate password
     if (empty($_POST["s-password"])) {
@@ -118,14 +71,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["s-father"])) {
         $errors["father_name"] = "Father's Name is required";
     } else {
-        $sfather = test_input($_POST["s-father"]);
+        $sfather = ucwords(test_input($_POST["s-father"]));
     }
 
     // Validate Mother's Name
     if (empty($_POST["s-mother"])) {
         $errors["mother_name"] = "Mother's Name is required";
     } else {
-        $smother = test_input($_POST["s-mother"]);
+        $smother = ucwords(test_input($_POST["s-mother"]));
     }
 
     // Validate Parents Contact
