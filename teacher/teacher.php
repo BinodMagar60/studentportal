8caa45bd317af6fdee476df40fde218d9fa4062a
ThamanGurung01 <tgurung519@gmail.com>
2024-01-26 20:01:52 +0545

    student & teacher logout

--- teacher/teacher.php
+++ teacher/teacher.php
@@ -2,6 +2,9 @@
 include "../php/config/sessionStart.php";
 if(isset($_SESSION['userType'])){
 echo "teacher homepage ".$_SESSION['userType'].' '.$_SESSION['userName'];
+?>
+<a href="../php/validate/logout.php">logout</a>
+<?php
 }else{
   echo 'not set';
 }
