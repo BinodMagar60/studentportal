53eed089b9142fc30adbb137a9b720babd290577
ThamanGurung01 <tgurung519@gmail.com>
2024-01-30 08:08:02 +0545

    delete popup updated

--- admin/admin-events.php
+++ admin/admin-events.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,15 +34,16 @@
 
                 </table>
                 <button type="submit" class="submit-btn">submit</button>
-                
+                </fieldset>
+        </form>
                 <div class="title title1 title21">Event List</div>
                 <div class="eventlist">
-                  <!-- <?php 
-                //   require_once "../php/config/db.php";
-                //   $sql= "SELECT * FROM notice ORDER BY n_date ASC";
-                //   $exesql= mysqli_query($con,$sql);
-                //   if(mysqli_num_rows($exesql)!=0){
-                //     $i=0;
+                  <?php 
+                  require_once "../php/config/db.php";
+                  $sql= "SELECT * FROM notice ORDER BY n_date ASC";
+                  $exesql= mysqli_query($con,$sql);
+                  if(mysqli_num_rows($exesql)!=0){
+                    $i=0;
                     ?>
                     <table id="tableBox">
                     <tr>
@@ -52,29 +54,29 @@
                     </tr>
                     
                         <?php
-                //   while($result_notice=mysqli_fetch_assoc($exesql)){
-                //     $i+= 1;
-                //   $dbDate = $result_notice['n_date'];
-                //   $dateObject = date_create($dbDate);
-                //   $formattedDate = date_format($dateObject, 'M d');
+                  while($result_notice=mysqli_fetch_assoc($exesql)){
+                    $i+= 1;
+                  $dbDate = $result_notice['n_date'];
+                  $dateObject = date_create($dbDate);
+                  $formattedDate = date_format($dateObject, 'M d');
                     ?>
                     <tr>
-                    <td><?php //if(isset($i)) echo $i;?></td>
-                    <td><?php //if(isset($result_notice['notice'])) echo $result_notice['notice'];?></td>
-                    <td><?php //if(isset($formattedDate)) echo $formattedDate;?></td>
+                    <td><?php if(isset($i)) echo $i;?></td>
+                    <td><?php if(isset($result_notice['notice'])) echo $result_notice['notice'];?></td>
+                    <td><?php if(isset($formattedDate)) echo $formattedDate;?></td>
                     <td id="btn-style">
                      <button type="button" style="background-color: green;" onclick="updateEvents();">Update</button>
-                     <button type="button" style="background-color: red;" onclick="deleteEvents();">Delete</button>
+                     <button type="button" style="background-color: red;" onclick="deleteEvent(<?php echo $result_notice['id']; ?>);">Delete</button>
                     </td>
                       </tr>
                     <?php
-                //   }
-                //   }else{
-                //     echo "no data";
-                //   }
+                  }
+                  }else{
+                    echo "no data";
+                  }
                   ?>
                       
-                    </table> -->
+                    </table>
                 </div>
 
 
@@ -103,21 +105,38 @@
                 </div>
 
 
-                <div class="deleteevent" id="deleteevent">
+                <!-- <div class="deleteevent" id="deleteevent">
                     <div>
                         <div class="deletelogo"><i class="fa-regular fa-circle-question" style="color: #ff0000;"></i></div>
                         <div class="confirmation">Are you sure you want to delete it?</div>
                         <div class="btn-delete-event">
-                            <a href="../php/event/deleteEvent.php?uid=<?php if(isset($result_notice)) echo $result_notice['id']?>"><button style="background-color: red;">Yes</button></a>
+                           <button style="background-color: red;">Yes</button></a>
                             <button type="button" style="background-color: gray;" onclick="deleteCancel();">No</button>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
-            </fieldset>
-        </form>
+
        
     </div>
-    <script src="../js/admin-updateevents.js"></script>
+    <script>
+        function deleteEvent(eventId) {
+            console.log(eventId);
+    var confirmDelete = confirm("Are you sure you want to delete this event?");
+    if (confirmDelete) {
+        // Send AJAX request to the server to delete the event
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "../php/event/deleteEvent.php?uid=" + eventId, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Handle the response if needed
+                // Reload the page or update the event list
+            }
+        };
+        xhr.send();
+    }
+}
+    </script>
+    <!-- <script src="../js/admin-updateevents.js"></script> -->
 </body>
 </html>
