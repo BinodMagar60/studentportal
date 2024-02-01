418151291fe121ae2001862cd6712dbadbcab409
ThamanGurung01 <tgurung519@gmail.com>
2024-02-01 16:09:27 +0545

    add announcement small change

--- php/announcement/add_announcement.php
+++ php/announcement/add_announcement.php
@@ -4,8 +4,13 @@ require_once "announcementValidate.php";
 if($_SERVER['REQUEST_METHOD']==='POST'){
 if (empty($errors)) {
 if(isset($description,$exp_date,$user_whom)){
- 
+ if($user_whom == "everyone" || $user_whom == "teacher"){
+  $sql= "INSERT INTO announcements(a_description,exp_date,user_whom,user_class,user_section) VALUES ('$description','$exp_date','$user_whom','-','-')";
+ }else if($user_whom=="student"){
 $sql= "INSERT INTO announcements(a_description,exp_date,user_whom,user_class,user_section) VALUES ('$description','$exp_date','$user_whom','$user_class','$user_section')";
+ }else{
+  echo "user_whom error";
+ }
 if(mysqli_query($con,$sql)){
   // echo "data inserted successfully";.
       // header("location:../../admin/admin.php");
