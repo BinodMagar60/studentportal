c42c86332bc2fd7bf501285fa490681db5dbb81c
ThamanGurung01 <tgurung519@gmail.com>
2024-02-03 10:58:45 +0545

    poster_name for announcement

--- php/announcement/add_announcement.php
+++ php/announcement/add_announcement.php
@@ -1,13 +1,18 @@
 <?php
+require_once "../config/sessionStart.php";
+require_once "../config/db.php";
+if(isset($_SESSION['userName'])){
+  $userName=$_SESSION['userName'];
+}
 require_once "../config/db.php";
 require_once "announcementValidate.php";
 if($_SERVER['REQUEST_METHOD']==='POST'){
 if (empty($errors)) {
 if(isset($description,$exp_date,$user_whom)){
  if($user_whom == "everyone" || $user_whom == "teacher"){
-  $sql= "INSERT INTO announcements(a_description,exp_date,user_whom,user_class,user_section) VALUES ('$description','$exp_date','$user_whom','-','-')";
+  $sql= "INSERT INTO announcements(a_description,exp_date,user_whom,user_class,user_section,poster_name) VALUES ('$description','$exp_date','$user_whom','-','-','$userName')";
  }else if($user_whom=="student"){
-$sql= "INSERT INTO announcements(a_description,exp_date,user_whom,user_class,user_section) VALUES ('$description','$exp_date','$user_whom','$user_class','$user_section')";
+$sql= "INSERT INTO announcements(a_description,exp_date,user_whom,user_class,user_section,poster_name) VALUES ('$description','$exp_date','$user_whom','$user_class','$user_section','$userName')";
  }else{
   echo "user_whom error";
  }
