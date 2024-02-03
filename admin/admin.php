bb616a47fcda6f213cf50bf9b9b5230db2cadd62
ThamanGurung01 <tgurung519@gmail.com>
2024-02-03 11:03:18 +0545

    announcement show changed exp date to poster name

--- admin/admin.php
+++ admin/admin.php
@@ -201,9 +201,6 @@ $sql_announcement= "SELECT * FROM announcements ORDER BY created_date ASC";
 $exesql_announcement= mysqli_query($con,$sql_announcement);
 if(mysqli_num_rows($exesql_announcement)!=0){
   while($result_announcement=mysqli_fetch_assoc($exesql_announcement)){
-  $expDate = $result_announcement['exp_date'];
-  $a_exp_dateObject = date_create($expDate);
-  $a_exp_formattedDate = date_format($a_exp_dateObject, 'M d');
 
   $createdDate=$result_announcement['created_date'];
   $a_created_dateObject = date_create($createdDate);
@@ -211,7 +208,7 @@ if(mysqli_num_rows($exesql_announcement)!=0){
   ?>
   <span><?php if(isset($a_created_formattedDate)) echo $a_created_formattedDate;?></span><br>
   <span><?php if(isset($result_announcement['a_description'])) echo $result_announcement['a_description']; ?></span><br>
-  <span><?php if(isset($a_exp_formattedDate)) echo $a_exp_formattedDate;?></span><br>
+  <span><?php if(isset($result_announcement['poster_name'])) echo $result_announcement['poster_name']; ?></span><br>
   <?php
 }
 }else{
