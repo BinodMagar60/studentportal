bd94d0db248145192591eadb43d42dcc6a2ba203
BinodMagar60 <kauchabinod88@gmail.com>
2024-02-07 17:16:32 +0545

    teacher and student profiles

---
+++ teacher/html/Studentprofile-profile.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/studentprofile-profile-style.css">
+</head>
+<body>
+  <!-- -->
+    <form action="" method="post" enctype="multipart/form-data" novalidate autocomplete="off">
+        <div class="basic-info">
+            <div class="topname">
+                Student Informaition
+            </div>
+            <div class="myinfo-container">
+                <div class="info-photo">
+                  <div class="intro">Profile Picture</div>
+                    <div class="myimg">
+                        <div class="circleline" id="imageContainer">
+                          <img src="" alt="Student photo" id="uploadedImage">
+                        </div>
+                    </div>
+                </div>
+                <div class="info-me">
+                  <div class="intro">Basic Information</div>
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Gender</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Date of Birth</td>
+                            <td>Thaman A.D</td>
+                        </tr>
+                        <tr>
+                            <td>Contact</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td id="email">Thaman</td>
+                        </tr>
+                        <tr>
+                          <td>User Type</td>
+                          <td>Student</td>
+                        </tr>
+                    </table>
+
+                    <div class="intro">Acadamic Information</div>
+                    <table>
+                        <tr>
+                            <td>Class</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Section</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Attendance</td>
+                            <td>-</td>
+                        </tr>
+                    </table>
+
+
+
+                    <div class="intro">Parent Information</div>
+                    <table>
+                        <tr>
+                            <td>Father's Name</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Mother's Name</td>
+                            <td>Thaman</td>
+                        </tr>
+                        <tr>
+                            <td>Parent's Contact</td>
+                            <td>Thaman</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+
+
+    </form>
+
+</body>
+</html>
---
+++ teacher/html/Teacherprofile-profile.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/Teacherprofile-profile-style.css">
+</head>
+<body>
+  <!-- -->
+    <form action="" method="post" enctype="multipart/form-data" novalidate autocomplete="off">
+        <div class="basic-info">
+            <div class="topname">
+                Teacher Informaition
+            </div>
+            <div class="myinfo-container">
+                <div class="info-photo">
+                  <div class="intro">Profile Picture</div>
+                    <div class="myimg">
+                        <div class="circleline"  id="imageContainer">
+                          <img src="" alt="Teacher photo" id="uploadedImage">
+                        </div>
+                    </div>
+                </div>
+                <div class="info-me">
+                  <div class="intro">Basic Information</div>
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td>Thaman Gurung</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>Thaman Gurung</td>
+                        </tr>
+                        <tr>
+                            <td>Gender</td>
+                            <td>Thaman Gurung</td>
+                        </tr>
+                        <tr>
+                            <td>Date of Birth</td>
+                            <td>Thaman Gurung A.D</td>
+                        </tr>
+                        <tr>
+                            <td>Contact</td>
+                            <td>Thaman Gurung</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td id="email">ThamanGurung@gmail.com</td>
+                        </tr>
+                        <tr>
+                          <td>User Type</td>
+                          <td>Teacher</td>
+                        </tr>
+                        <tr>
+                            <td>Assigned Subject</td>
+                            <td>Thama Gurung</td>
+                        </tr>
+                        
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+
+
+    </form>
+  
+</body>
+</html>
