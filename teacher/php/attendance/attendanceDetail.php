38b7ba1016d5bffbd30ece31cc46b9fc4e3e3230
ThamanGurung01 <tgurung519@gmail.com>
2024-02-17 10:27:32 +0545

    addAttendance

--- teacher/php/attendance/attendanceDetail.php
+++ teacher/php/attendance/attendanceDetail.php
@@ -1,20 +1,13 @@
-
-
-
-
-
-
 <?php
 require_once "../../../php/config/db.php";
 $class = isset($_GET['class']) ? $_GET['class'] : 'one';
 $section = isset($_GET['section']) ? $_GET['section'] : 'A';
                     $sql = "SELECT * FROM student_table WHERE class='$class' AND section='$section' ORDER BY name ASC";
-
 if ($exesql = mysqli_query($con, $sql)) {
     if (mysqli_num_rows($exesql) > 0) {
         $i = 0;
         ?>
-        <form >
+        <form action="../php/attendance/addAttendance.php" method="post">
                 <div class="today-attendance"><span><input type="checkbox" id="today-atten"></span><label for="today-atten">Todays Attendance</label> </div>
 
                 <table id="attendanceTable">
@@ -34,23 +27,27 @@ if ($exesql = mysqli_query($con, $sql)) {
                         <td><?php if(isset($i)) echo $i;?></td>
                         <td><?php if(isset($searchResult['name'])) echo $searchResult['name'];?></td>
                         <td><?php if(isset($searchResult['email'])) echo$searchResult['email'];?></td>
-                        <td><input type="checkbox"></td>
+                        <!-- <input type="hidden" name="i<?php //if(isset($i)) echo $i;?>" value="<?php //if(isset($i)) echo $i;?>"> -->
+                        <input type="hidden" name="s_id<?php if(isset($i)) echo $i;?>" value="<?php if(isset($searchResult['id'])) echo $searchResult['id'];?>">
+                        <input type="hidden" name="s_name<?php if(isset($i)) echo $i;?>" value="<?php if(isset($searchResult['name'])) echo $searchResult['name'];?>">
+                        <input type="hidden" name="s_email<?php if(isset($i)) echo $i;?>" value="<?php if(isset($searchResult['email'])) echo$searchResult['email'];?>">
+                        <input type="hidden" name="s_class<?php if(isset($i)) echo $i;?>" value="<?php if(isset($searchResult['class'])) echo $searchResult['class'];?>">
+                        <input type="hidden" name="s_section<?php if(isset($i)) echo $i;?>" value="<?php if(isset($searchResult['section'])) echo $searchResult['section'];?>">
+                        <td><input type="checkbox" name="attendance<?php if(isset($i)) echo $i;?>" value="P"></td>
                     </tr>
                    <?php
             }
-        ?>
+           ?> 
+           <input type="hidden" name="numofData" value="<?php if(isset($i)) echo $i;?>">
         <tr>
             <td colspan="4" style="border: none; background-color:white;">
                 <div class="attendance-btn-div">
-                    <button type="button" class="attendanceSubmit">Submit</button>
+                    <button class="attendanceSubmit">Submit</button>
                 </div>
             </td>
         </tr>
     </table>
-    
 </form>
-        
-        
         <?php
         }
         else{
