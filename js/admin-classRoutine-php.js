99ea3f22f71c5bbb06e13fa64d2f8f21faf264f7
BinodMagar60 <kauchabinod88@gmail.com>
2024-02-18 18:53:11 +0545

    little change ig

--- js/admin-classRoutine-php.js
+++ js/admin-classRoutine-php.js
@@ -4,16 +4,13 @@ function updateClassroutine(event){
     var formData = new FormData(document.getElementById('classroutinedaily'));
 
     $.ajax({
-        url: '../php/classRoutine/updateClassRoutine.php',
+        url: '../php/classRoutine/addClassRoutine.php',
         type: 'POST',
         data: formData,
         processData: false,
         contentType: false,
         success: function (response) {
             // console.log('Response:', response);
-           
-
-            
         },
         error: function (xhr, status, error) {
             console.error('Error:', status, error);
