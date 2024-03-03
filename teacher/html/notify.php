c0bfd590e2d5db2cd7d79e4212ab9b57ef6c5d36
ThamanGurung01 <tgurung519@gmail.com>
2024-03-03 09:08:16 +0545

    notify

--- teacher/html/notify.php
+++ teacher/html/notify.php
@@ -88,8 +88,8 @@
                     $notifyListSql="SELECT * FROM teacher_notify";
                     if($notifyExe=mysqli_query($con,$notifyListSql)){
                         if(mysqli_num_rows($notifyExe)>0){
+                            $i=1;
                             while($notifyResult=mysqli_fetch_assoc($notifyExe)){
-$i=1;
                                 ?>
                     <tr>
                         <td><?php if(isset($i)) echo $i?></td>
@@ -99,13 +99,22 @@ $i=1;
                                 <button class="btn-notify" style="background-color: green;"  onclick="notifyUpdate();">Update</button>
                                 <button class="btn-notify" style="background-color: red;" onclick="notifyDelete();">Delete</button>
                             </div>
+                            <div class="notify-delete" id="notify-delete">
+        <div class="title-logo-delete"><i class="ri-question-line"></i></div>
+        <div class="title">Are you sure you want to delete it?</div>
+        <div class="btn-delete-notify">
+            <a href="../php/notify/deleteNotify.php?uid=<?php if(isset($notifyResult['id'])) echo $notifyResult['id']?>"><button class="delete-btn-notify" style="background-color: red;" onclick="notifyDeletePopup();">Yes</button></a>
+            <button class="delete-btn-notify" style="background-color: gray;" onclick="notifyDeleteRemove();">No</button>
+        </div>
                         </td>
                     </tr>
                     <?php
+                    $i++;
                             }
                         }
                     }
                     ?>
+    </div>
                 </table>
             </div>
         </fieldset>
@@ -131,14 +140,7 @@ $i=1;
 
 
 
-    <div class="notify-delete" id="notify-delete">
-        <div class="title-logo-delete"><i class="ri-question-line"></i></div>
-        <div class="title">Are you sure you want to delete it?</div>
-        <div class="btn-delete-notify">
-            <button class="delete-btn-notify" style="background-color: red;" onclick="notifyDeletePopup();">Yes</button>
-            <button class="delete-btn-notify" style="background-color: gray;" onclick="notifyDeleteRemove();">No</button>
-        </div>
-    </div>
+
 
 
 
